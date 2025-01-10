8133f72bc23e8ffeb6144602e4edbb7ac21d2608
Martin Marsal <mmarsal@stud.hs-heilbronn.de>
2025-01-10 12:48:43 +0100

    Make multiple files uploadable from webapp folder

--- google_storage_bucket_objects.tf
+++ google_storage_bucket_objects.tf
@@ -1,7 +1,30 @@
+resource "google_storage_bucket_object" "web_files" {
+  for_each = fileset("${path.module}/webapp", "**")
+
+  name   = each.key
+  bucket = google_storage_bucket.static_website.name
+  source = "${path.module}/webapp/${each.key}"
+
+  content_type = lookup(
+  {
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "png"  = "image/png",
+    "jpg"  = "image/jpeg",
+  },
+  split(".", each.key)[length(split(".", each.key)) - 1],
+  "application/octet-stream"
+  )
+
+  depends_on = [
+    local_file.generated_index_html]
+}
+
 resource "google_storage_bucket_object" "index_html" {
   name = "index.html"
   bucket = google_storage_bucket.static_website.name
-  source = "${path.module}/generated/index.html"
+  source = "${path.module}/webapp/index.html"
   content_type = "text/html"
 
   depends_on = [
--- local_files.tf
+++ local_files.tf
@@ -1,5 +1,5 @@
 resource "local_file" "generated_index_html" {
-  filename = "${path.module}/generated/index.html"
+  filename = "${path.module}/webapp/index.html"
   content = templatefile("${path.module}/webapp/index.html.tmpl", {
     api_url = google_cloud_run_v2_service.api_container.uri,
     custom_api_url = google_cloud_run_v2_service.custom_api_container.uri
